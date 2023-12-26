7ef0ee720301370291596eda2423e740e18e3d95
Milan Andric <mandric@gmail.com>
2023-12-26 16:33:48 -0600

    wip: tests should fail immediately

--- tests/base_test.sh
+++ tests/base_test.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env sh
+# shellcheck disable=SC3040,SC3043
+# See `help set` for more information
+set -o xtrace
+set -o errexit
+set -o nounset
+if set -o | grep pipefail >/dev/null; then
+   set -o pipefail
+fi
 
 ###############################################################################
 ### Show
 ###############################################################################
 out="$(./do.sh show foo bar baz)"
+test "$?" == "0"
 test "$out" ==  "I am showing foo bar baz"
