9951b5382727b792ba84f6a191cc072d1c13b367
Milan Andric <mandric@gmail.com>
2023-12-27 09:25:39 -0600

    wip: add bash tests

---
+++ tests/bash_test.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+testUsageOutput() {
+  local out
+  local expected
+  out="$(
+    docker run --rm -v "$(pwd)/do.sh:/do.sh" ubuntu bash /do.sh
+  )"
+  expected="$(
+    printf "\nUsage:\n\t./do.sh (all|lint|build|testAll|deploy|show)\n"
+  )"
+  test "$out" == "$expected"
+}
+
+testSuccess() {
+  docker run --rm -v "$(pwd)/do.sh:/do.sh" ubuntu bash /do.sh deploy
+  test "$?" == "0"
+}
+
+testFailure() {
+  out="$(
+    docker run --rm -v "$(pwd)/do.sh:/do.sh" ubuntu bash /do.sh _fail foo || printf "foo failed"
+  )"
+  test "$out" == "$(printf "Failed: foo\nfoo failed")"
+}
